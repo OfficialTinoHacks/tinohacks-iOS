4a27f8efe5e1e8e5026683aea804d1f3228e2ee5
kashyappanda <kashyap.panda@gmail.com>
2017-03-09 12:17:59 -0800

    Revert "Updated UI"
    This reverts commit bad935da10367747ed38be44857fbbd8db0a4903.

---
+++ TinoHacksApp/AwardsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AwardsViewController.swift
+//  TinoHacksApp
+//
+//  Created by Kashyap Panda on 2/25/17.
+//  Copyright © 2017 Tino Hacks. All rights reserved.
+//
+
+import UIKit
+
+class AwardsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
