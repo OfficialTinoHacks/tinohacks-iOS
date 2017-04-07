823fe184590c25f6baac7fa979b89678598b91f6
kashyappanda <kashyap.panda@gmail.com>
2017-04-07 16:26:57 -0700

    Updated tab bar icons

--- TinoHacksApp/AppDelegate.swift
+++ TinoHacksApp/AppDelegate.swift
@@ -24,6 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         print(userInfo)
+        
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
---
+++ TinoHacksApp/NewsTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  NewsTableViewController.swift
+//  TinoHacksApp
+//
+//  Created by Kashyap Panda on 4/7/17.
+//  Copyright © 2017 Tino Hacks. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
